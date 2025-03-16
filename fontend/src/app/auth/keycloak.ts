0a70fed5f486a3bb3b81ed4019cdbe9d9f7cb4b8
Nông Tiến Sơn <68463083+nongtiensonpro@users.noreply.github.com>
2025-03-16 22:51:54 +0700

    Update keycloak.ts

--- fontend/src/app/auth/keycloak.ts
+++ fontend/src/app/auth/keycloak.ts
@@ -4,7 +4,8 @@ const keycloakConfig = {
     url: 'http://localhost:9082', // Đảm bảo URL này đúng
     realm: 'YellowCatCompany',
     clientId: 'YellowCatCompanyWeb',
-    redirectUri: 'http://localhost:9082/realms/YellowCatCompany/broker/google/endpoint'
+    redirectUri: 'http://localhost:3000',
+    client_secret: 'tc2vierNhQ0BsIIkBXzvyHVY61SItzsU'
 };
 
 const keycloak = new Keycloak(keycloakConfig);
