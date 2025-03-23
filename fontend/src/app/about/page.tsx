61a6adb1f7aa67b98fbccd60394502b09967794c
Nông Tiến Sơn <68463083+nongtiensonpro@users.noreply.github.com>
2025-03-23 23:53:13 +0700

    Delete fontend directory

--- fontend/src/app/about/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function About(){
-    return(
-        <div>
-            <h1>Welcome to the About!</h1>
-        </div>
-    )
-}
--- fontend/src/app/services/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Service(){
-    return(
-        <div>
-            <h1>Welcome to the Service!</h1>
-        </div>
-    )
-}
