65b358760b10611d972a29381b24d89f1d0ce223
Nông Tiến Sơn <68463083+nongtiensonpro@users.noreply.github.com>
2025-03-29 11:31:19 +0700

    Thêm Demo Upload ảnh

--- fontend/app/page.tsx
+++ fontend/app/page.tsx
@@ -1,9 +1,28 @@
+"use client"
 import { title, subtitle } from "@/components/primitives";
 import VNPAY from "@/components/vnpay-demo"
-
+import { CldImage } from 'next-cloudinary';
+import UploadImage from "@/components/upload"
+import {Table, TableBody, TableCell, TableColumn, TableHeader, TableRow} from "@heroui/react";
 export default function Home() {
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+      <Table aria-label="Example static collection table">
+        <TableHeader>
+          <TableColumn>Thanh toán</TableColumn>
+          <TableColumn>Upload ảnh</TableColumn>
+        </TableHeader>
+        <TableBody>
+          <TableRow key="1">
+            <TableCell>
+              <VNPAY/>
+            </TableCell>
+            <TableCell>
+              <UploadImage />
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
       <div className="inline-block max-w-xl text-center justify-center">
         <span className={title()}>I&#39;ll&nbsp;</span>
         <span className={title({ color: "violet" })}> make him&nbsp;</span>
@@ -14,7 +33,8 @@ export default function Home() {
         <div className={subtitle({ class: "mt-4" })}>
           Don Vito Corleone.
         </div>
-        <VNPAY/>
+
+
       </div>
     </section>
   );
--- fontend/next.config.js
+++ fontend/next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    env: {
+        NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+        NEXT_PUBLIC_CLOUDINARY_API_KEY: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
+    },
+};
 
 module.exports = nextConfig;
