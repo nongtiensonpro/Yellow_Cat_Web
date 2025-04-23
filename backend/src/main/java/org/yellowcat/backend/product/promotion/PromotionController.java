06d3336c48a14dee482f3619f1710d2faa2de622
huyldph <ledinhhuy26032004@gmail.com>
2025-04-23 14:28:18 +0700

    feat(controller): add PromotionController with CRUD and pagination

---
+++ backend/src/main/java/org/yellowcat/backend/product/promotion/PromotionController.java
@@ -0,0 +1,59 @@
+package org.yellowcat.backend.product.promotion;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.yellowcat.backend.common.config_api.response.PageResponse;
+import org.yellowcat.backend.common.config_api.response.ResponseEntityBuilder;
+import org.yellowcat.backend.product.promotion.dto.PromotionRequest;
+import org.yellowcat.backend.product.promotion.dto.PromotionResponse;
+
+@RestController
+@RequestMapping("/api/promotions")
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class PromotionController {
+    PromotionService promotionService;
+
+    @GetMapping
+    ResponseEntity<?> getPromotions(
+            @RequestParam(defaultValue = "0") Integer page,
+            @RequestParam(defaultValue = "10") Integer size) {
+        Pageable pageable = PageRequest.of(page, size);
+        PageResponse<PromotionResponse> response = new PageResponse<>(promotionService.getAll(pageable));
+
+        return ResponseEntityBuilder.success(response);
+    }
+
+    @GetMapping("/{id}")
+    ResponseEntity<?> getPromotionById(@PathVariable Integer id) {
+        return ResponseEntityBuilder.success(promotionService.getById(id));
+    }
+
+    @PostMapping
+    @PreAuthorize("hasAnyAuthority('Admin_Web')")
+    ResponseEntity<?> createPromotion(@RequestBody PromotionRequest request) {
+        PromotionResponse response = promotionService.create(request);
+
+        return ResponseEntityBuilder.success(response);
+    }
+
+    @PutMapping("/{id}")
+    @PreAuthorize("hasAnyAuthority('Admin_Wed')")
+    ResponseEntity<?> updatePromotion(@PathVariable Integer id, @RequestBody PromotionRequest request) {
+        PromotionResponse response = promotionService.update(id, request);
+
+        return ResponseEntityBuilder.success(response);
+    }
+
+    @DeleteMapping("/{id}")
+    ResponseEntity<Boolean> deletePromotion(@PathVariable Integer id) {
+
+        return ResponseEntity.ok(promotionService.delete(id));
+    }
+}
