2865f0949bce963a238b1fce7d008c4e2dcc60f8
huyldph <ledinhhuy26032004@gmail.com>
2025-06-07 21:28:37 +0700

    created method findByOrder_OrderId and findByIdWithOrder

--- backend/src/main/java/org/yellowcat/backend/product/orderItem/OrderItemRepository.java
+++ backend/src/main/java/org/yellowcat/backend/product/orderItem/OrderItemRepository.java
@@ -1,9 +1,16 @@
 package org.yellowcat.backend.product.orderItem;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
 @Repository
 public interface OrderItemRepository extends JpaRepository<OrderItem, Integer> {
+    List<OrderItem> findByOrder_OrderId(Integer orderOrderId);
+
+    @Query("SELECT oi FROM OrderItem oi JOIN FETCH oi.order WHERE oi.orderItemId = :id")
+    OrderItem findByIdWithOrder(@Param("id") Integer id);
 }
