13807e5327bbf0f9833f718529b5f91d6e42c044
huyldph <ledinhhuy26032004@gmail.com>
2025-06-08 10:04:11 +0700

    Create API for getOrders and getOrderByStatus

--- backend/src/main/java/org/yellowcat/backend/product/order/OrderController.java
+++ backend/src/main/java/org/yellowcat/backend/product/order/OrderController.java
@@ -3,10 +3,13 @@ package org.yellowcat.backend.product.order;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import org.yellowcat.backend.common.config_api.response.PageResponse;
 import org.yellowcat.backend.common.config_api.response.ResponseEntityBuilder;
+import org.yellowcat.backend.product.order.dto.OrderResponse;
 import org.yellowcat.backend.product.order.dto.OrderUpdateRequest;
 import org.yellowcat.backend.product.order.dto.OrderUpdateResponse;
 
@@ -16,6 +19,30 @@ import org.yellowcat.backend.product.order.dto.OrderUpdateResponse;
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class OrderController {
     OrderService orderService;
+
+    @GetMapping()
+    @PreAuthorize("hasAnyAuthority('Admin_Web', 'Staff_Web')")
+    public ResponseEntity<?> getOrders(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size
+    ) {
+        Page<OrderResponse> orders = orderService.getOrders(page, size);
+        PageResponse<OrderResponse> pageResponse = new PageResponse<>(orders);
+
+        return ResponseEntityBuilder.success(pageResponse);
+    }
+
+    @GetMapping("/status")
+    @PreAuthorize("hasAnyAuthority('Admin_Web', 'Staff_Web')")
+    public ResponseEntity<?> getOrderByStatus(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size
+    ) {
+        Page<OrderResponse> orders = orderService.getOrderByStatus(page, size);
+        PageResponse<OrderResponse> pageResponse = new PageResponse<>(orders);
+
+        return ResponseEntityBuilder.success(pageResponse);
+    }
 
     @PostMapping
     @PreAuthorize("hasAnyAuthority('Admin_Web', 'Staff_Web')")
